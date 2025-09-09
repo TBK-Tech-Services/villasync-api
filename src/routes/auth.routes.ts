f928122e2791b9c19b76ccf6a24ff82cde58be7f
sahil ladhania <sahilladhania@sahils-MacBook-Air.local>
2025-09-09 18:50:41 +0530

    feat: update login controller and service

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { changePassword, createAdmin, forgotPassword, logoutUser } from "../controllers/auth.controllers.ts";
+import { changePassword, createAdmin, forgotPassword, loginUser, logoutUser } from "../controllers/auth.controllers.ts";
 
 const router = Router();
 
 router.post("/create-admin", createAdmin);
-// router.post("/login", loginUser);
+router.post("/login", loginUser);
 router.post("/logout", logoutUser);
 router.post("/forgot-password", forgotPassword);
 router.post("/change-password", changePassword);  
--- src/services/auth.services.ts
+++ src/services/auth.services.ts
@@ -1,5 +1,41 @@
 import prisma from "../db/DB.ts";
+import type { User } from "../types/general/userData.ts";
 import { createAdminSchema, type createAdminData, } from "../validators/data-validators/auth/createAdmin.ts";
+
+export async function getUserService({ email } : {email : string}): Promise<User | null> {
+  try {
+    const user = await prisma.user.findUnique({
+      where : {
+        email : email
+      },
+      select : {
+        firstName : true,
+        lastName : true,
+        email : true,
+        password : true,
+        role : {
+          select : {
+            name : true
+          }
+        }
+      }
+    })
+
+    if(!user){
+      return null;
+    }
+
+    return {
+      ...user,
+      role : user.role?.name ?? ""
+    };
+  }
+  catch (error) {
+    const message = error instanceof Error ? (error.message) : String(error);
+    console.error(`Error while getting a User : ${message}`);
+    throw new Error(`Error while getting a User : ${message}`);
+  }
+}
 
 export async function getAdminService({ email , role } : {email : string , role : string}): Promise<createAdminData | null> {
   try {
