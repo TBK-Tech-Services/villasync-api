4ca2a12861f9b7f3407e8c24123931b37130de8f
sahil ladhania <sahilladhania@sahils-MacBook-Air.local>
2025-10-08 14:39:01 +0530

    fix: done some changes

--- src/validators/data-validators/auth/login.ts
+++ src/validators/data-validators/auth/login.ts
@@ -1,8 +1,15 @@
 import z from 'zod';
 
 export const loginSchema = z.object({
-    email: z.string(),
-    password: z.string().min(8, "Password must be at least 8 characters"),
+    email: z
+            .string()
+            .min(1, "Email is required")
+            .toLowerCase()
+            .trim(),
+    password: z
+            .string()
+            .min(1, "Password is required")
+            .min(8, "Password must be at least 8 characters"),
 });
 
 export type LoginData = z.infer<typeof loginSchema>;
